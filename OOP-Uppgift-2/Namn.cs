c87939f669339d5bdd9d0a9abb5154ab5ddadb57
HerrBergenholtz <zbdv8800@gmail.com>
2024-04-03 10:54:53 +0200

    Add project files.

---
+++ OOP-Uppgift-2/Namn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Uppgift_2
+{
+    internal class Namn
+    {
+        private string förnamn;
+        private string efternamn;
+
+        public string SetNamn
+        {
+            get { return förnamn + " " + efternamn; }
+            set { förnamn = value.Split(" ")[0]; efternamn = value.Split(" ")[1]; }
+        }
+    }
+}
---
+++ OOP-Uppgift-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using OOP_Uppgift_2;
+using System;
+
+namespace Uppgift2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Namn namn = new();
+
+            namn.SetNamn = "Zacharias Bergenholtz";
+            Console.WriteLine(namn.SetNamn);
+        }
+    }
+}
